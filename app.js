0e33c610d5fed8464e45cbe734dbd2644d45d74b
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-08-18 22:03:55 +0530

    Customer CRUD Done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const user = require('./routes/user')
 const item = require('./routes/item')
 const customer = require('./routes/customer')
+const order = require('./routes/order')
+const orderDetail = require('./routes/orderDetail')
 
 const app = express()
 const port = 4000
@@ -11,12 +13,9 @@ app.use(express.json())
 app.use('/customer', customer)
 app.use('/item',  item)
 app.use('/users', user)
+app.use('/order', order)
+app.use('/orderDetail', orderDetail)
 
-
-// app.get('/', (req, res) => {
-//     console.log('get request comming!');
-//     res.send('get req came for / route')
-// })
 
 app.listen(port, () => {
     console.log(`app starting on ${port}`);
